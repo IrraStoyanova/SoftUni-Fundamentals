8f9072d10289befc887724e503f1403e2bdb5815
IrinaStoyanova <irina_dimitrova77@gmail.com>
2022-05-26 15:44:35 +0300

    Deleted some mistakenly put exercises from Basics

--- FirstStepsInCoding/src/HelloSoftUni.java
+++
@@ -1,5 +0,0 @@
-public class HelloSoftUni {
-    public static void main(String[] args) {
-        System.out.println("Hello SoftUni");
-    }
-}
--- Fundamentals/src/DivisibleBy3.java
+++
@@ -1,9 +0,0 @@
-public class DivisibleBy3 {
-    public static void main(String[] args) {
-
-        for (int i = 3; i <=100 ; i+=3) {
-            System.out.println(i);
-
-        }
-    }
-}
